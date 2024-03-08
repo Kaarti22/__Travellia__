b3027628ad542d5f7278d12b8ca3252042ccf6b1
Kaarti22 <kaartimondi@gmail.com>
2024-03-08 13:57:18 +0530

    added assets and login page ui

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import { Providers } from "./providers";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Travellia - Scrapper",
+  description: "This app is powered by Bright Data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
